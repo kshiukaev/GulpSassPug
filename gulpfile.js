bf6ea6c6cb6694f9abf0c5b08823d4bee589b50d
Konstantin Shiukaev <kshiukaev@mail.ru>
2017-12-05 00:27:44 +0300

    Pug fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const 	gulp = require("gulp"),
 		sass = require("gulp-sass"),
 		pug = require("gulp-pug"),
-		//autoprefixer = require("gulp-autoprefixer"),
-		browserSync = require("browser-sync");
+		autoprefixer = require("gulp-autoprefixer"),
+		browserSync = require("browser-sync").create();
 
 
 gulp.task('pug', ['sass'], function(){
@@ -11,28 +11,15 @@ gulp.task('pug', ['sass'], function(){
 	.pipe(gulp.dest('./'))
 	.pipe(browserSync.reload({stream: true}))
 });
-gulp.task('sass', function() {
-    return multipipe(
-        gulp.src('app/sass/**/*.sass'),
-        (sourcemaps.init()),
-        (sass({outputStyle: 'expanded'})),
-        (rename({suffix: '.min', prefix : ''})),
-        (autoprefixer(['last 15 versions'])),
-        (cleanCSS()),
-        // (gcmq()),
-        (sourcemaps.write()),
-        (gulp.dest('app/css')),
-        (browserSync.reload({stream: true}))
-    ).on('error', notify.onError());
-});
-/*
+
+
 gulp.task('sass', function() {
 	gulp.src('./sass/**//*.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('./css'))
 	.pipe(browserSync.reload({stream: true}))
 });
-*/
+
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
